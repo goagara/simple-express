9ebe27ea16a249f6fd3f2702fd7f390bec36c931
bryantobing12 <bryantobing0@gmail.com>
2021-03-05 12:58:18 +0700

    Feat: add connection to the database

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const PORT = process.env.PORT || 3000;
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://34.101.236.8:27017/simple-express', {
+    auth: {
+      user: 'bryantobing12',
+      password: 'terbaik123',
+    },
+    authSource: 'admin',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
@@ -8,6 +26,7 @@ app.get('/', (req, res) => {
     status: 'OK',
     message:
       'Simple Express and Hello There Update, turn this into more complicated',
+    update: 'I already connected to the database',
   });
 });
 
